571913ecb82a3de534ed6ecbd0594c8caa7907e5
Jorge Ezequiel Caballero <87038713+shorsho21@users.noreply.github.com>
2026-08-04 12:18:44 -0300

    test supabase

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { supabase } from './supabase';
 
 @Injectable()
 export class AppService {
+  async test() {
+
+    const { data, error } =
+        await supabase
+            .from('users')
+            .select('*');
+
+    console.log(data);
+
+}
   getHello(): string {
+    this.test();
+    
     return 'Hello World!';
   }
 }
